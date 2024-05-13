8e605e30b51721dcf36056a3a70ce754ce45eb75
Merna <mernaamir88@gmail.com>
2024-05-12 23:06:19 -0800

    edit

--- lib/universites.dart
+++ lib/universites.dart
@@ -24,90 +24,114 @@ data.addAll(querySnapshot.docs);
     // TODO: implement build
     return Scaffold(
 
-      body: Stack(
-        children: [
-          Container(
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    Image.asset(
-                      "assets/images/university.png",
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
-                    Positioned(
-                      bottom: 150,
-                      right: 20,
-                      child: Text(
-                        "قائمة الجامعات",
-                        style: TextStyle(fontSize: 28, color: Colors.white),
-                      ),
-                    ),
-                    Positioned(
-                   left: 10,
-                      top: 30,
-                      child: InkWell(
-                          onTap: (){
-                            Navigator.pop(context);
-                          },
-                          child: Icon(Icons.arrow_back,size: 30,color: Colors.white,))
-                    ),
-                    // Icon(Icons.arrow_back)
+      body: FutureBuilder(builder:(context,snapshot){
+          if(snapshot.connectionState==ConnectionState.waiting){
+          return Center(child: CircularProgressIndicator());
+        }
 
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Colors.white,
-              ),
-              height: 650,
-              child: Container(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: InkWell(
-                          onTap: (){
-                            Navigator.pushNamed(context, faculty.routeName);
-                          },
-                          child: Text(
-                            "جامعة القاهرة",
-                            style: TextStyle(
-                                fontSize: 30,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        style: ElevatedButton.styleFrom(
+        if(snapshot.hasError){
+          return Center(child: Text("Something went errorrrrrrrrr"));
+        }
+        if(data.isEmpty){
+          return Text("List is empty");
+        }
+return Expanded(child:
 
-                          minimumSize: Size(300, 60),
-                          backgroundColor: Colors.blue,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                        ),
-                      ),
-                    ),
+ListView.builder(itemBuilder: (context,index){
 
-                  ],
-                ),
 
-              ),
-            ),
-          )
-        ],
-      ),
+return Text("${data[index]['name']}");
+}));
+
+
+      } ,future: getData())
+
+
+
     );
   }
 }
+// Stack(
+// children: [
+// Container(
+// child: Column(
+// children: [
+// Stack(
+// children: [
+// Image.asset(
+// "assets/images/university.png",
+// width: double.infinity,
+// fit: BoxFit.cover,
+// ),
+// Positioned(
+// bottom: 150,
+// right: 20,
+// child: Text(
+// "قائمة الجامعات",
+// style: TextStyle(fontSize: 28, color: Colors.white),
+// ),
+// ),
+// Positioned(
+// left: 10,
+// top: 30,
+// child: InkWell(
+// onTap: (){
+// Navigator.pop(context);
+// },
+// child: Icon(Icons.arrow_back,size: 30,color: Colors.white,))
+// ),
+// // Icon(Icons.arrow_back)
+//
+// ],
+// ),
+// ],
+// ),
+// ),
+// Positioned(
+// bottom: 0,
+// left: 0,
+// right: 0,
+// child: Container(
+// decoration: BoxDecoration(
+// borderRadius: BorderRadius.circular(50),
+// color: Colors.white,
+// ),
+// height: 650,
+// child: Container(
+// child: Column(
+// children: [
+// Padding(
+// padding: const EdgeInsets.all(15.0),
+// child: ElevatedButton(
+// onPressed: () {},
+// child: InkWell(
+// onTap: (){
+// Navigator.pushNamed(context, faculty.routeName);
+// },
+// child: Text(
+// "جامعة القاهرة",
+// style: TextStyle(
+// fontSize: 30,
+// color: Colors.white,
+// fontWeight: FontWeight.bold),
+// ),
+// ),
+// style: ElevatedButton.styleFrom(
+//
+// minimumSize: Size(300, 60),
+// backgroundColor: Colors.blue,
+// shape: RoundedRectangleBorder(
+// borderRadius: BorderRadius.circular(15),
+// ),
+// ),
+// ),
+// ),
+//
+// ],
+// ),
+//
+// ),
+// ),
+// )
+// ],
+// ),
